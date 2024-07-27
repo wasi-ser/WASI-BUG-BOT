2b5b908b0d253727d117bf32e19cfa26737e2066
wasi-ser <itxmewasi@gmail.com>
2024-07-27 11:57:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//base by Musa Rajput
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: none
+//Instagram: ....
+//Telegram: t.me/Musa_x0
+//GitHub: ......
+//WhatsApp: +923347748038
+//want more free bot join my tg chsnnal
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
