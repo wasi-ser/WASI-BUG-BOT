0b708bfb932757f99f9f9690da73e413692f9de9
Akku <161572818+Akbarafhufdg@users.noreply.github.com>
2024-10-04 12:04:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
+global.ownernomer = "923197173398"
 global.ownername = "🐛 MR WASI "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
